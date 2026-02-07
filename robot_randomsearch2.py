bb8466ba57f8a9c4d010fc992110b768649f50cc
haya-termos <haya.termos@gmail.com>
2026-02-07 15:20:54 +0100

    debut tp2

--- robot_randomsearch2.py
+++ robot_randomsearch2.py
@@ -18,8 +18,8 @@ class Robot_player(Robot):
     best_score=float('-inf')
     best_trial=0
     done=0
-    prev_log_sum_of_rotation=0
-    prev_log_sum_of_translation=0
+    trialorientation=0
+    current_score=0
 
 
     x_0 = 0
@@ -39,6 +39,7 @@ class Robot_player(Robot):
 
     def reset(self):
         super().reset()
+        self.theta=random.random()*360
 
     def step(self, sensors, sensor_view=None, sensor_robot=None, sensor_team=None):
 
@@ -49,24 +50,26 @@ class Robot_player(Robot):
 
         # toutes les X itérations: le robot est remis à sa position initiale de l'arène avec une orientation aléatoire
         if self.trial<=500:
+            if self.trialorientation<=3 :
+                if self.iteration % self.it_per_evaluation == 0:
+                    if self.iteration > 0:
+                        print ("\tparameters           =",self.param)
+                        print ("\ttranslations         =",self.log_sum_of_translation,"; rotations =",self.log_sum_of_rotation) # *effective* translation/rotation (ie. measured from displacement)
+                        print ("\tdistance from origin =",math.sqrt((self.x-self.x_0)**2+(self.y-self.y_0)**2))
+                        print ("\tscore=",self.log_sum_of_translation*(1-abs(self.log_sum_of_rotation)))
 
-            if self.iteration % self.it_per_evaluation == 0:
-                if self.iteration > 0:
-                    print ("\tparameters           =",self.param)
-                    print ("\ttranslations         =",self.log_sum_of_translation,"; rotations =",self.log_sum_of_rotation) # *effective* translation/rotation (ie. measured from displacement)
-                    print ("\tdistance from origin =",math.sqrt((self.x-self.x_0)**2+(self.y-self.y_0)**2))
-                    print ("\tscore=",self.log_sum_of_translation*(1-abs(self.log_sum_of_rotation)))
-
-                    if self.log_sum_of_translation*(1-abs(self.log_sum_of_rotation))> self.best_score:
-                        self.best_score=self.log_sum_of_translation*(1-abs(self.log_sum_of_rotation))
-                        self.bestParam=list(self.param)
-                        self.best_trial=self.trial
-                    
-                self.param = [random.randint(-1, 1) for i in range(8)]
-                self.trial = self.trial + 1
-                print ("Trying strategy no.",self.trial)
-                self.iteration = self.iteration + 1
-                return 0, 0, True # ask for reset
+                        if self.log_sum_of_translation*(1-abs(self.log_sum_of_rotation))> self.best_score:
+                            self.best_score=self.log_sum_of_translation*(1-abs(self.log_sum_of_rotation))
+                            self.bestParam=list(self.param)
+                            self.best_trial=self.trial
+                        
+                    self.param = [random.randint(-1, 1) for i in range(8)]
+                    self.trial = self.trial + 1
+                    print ("Trying strategy no.",self.trial)
+                    self.iteration = self.iteration + 1
+                    return 0, 0, True # ask for reset
+                
+                
         else:
             if self.done==0:
                 print ("Meilleure stratégie:")
@@ -96,4 +99,3 @@ class Robot_player(Robot):
         self.iteration = self.iteration + 1        
 
         return translation, rotation, False
-  
